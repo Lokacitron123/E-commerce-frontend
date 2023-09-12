dcce6cba6ddebd08af9ef00013a83898508a075c
Johan Lindell <johan.lindell91@gmail.com>
2023-09-12 09:50:20 +0200

    cleaned up LoginRegisterPage

--- src/pages/loginregisterpage/LoginRegisterPage.jsx
+++ src/pages/loginregisterpage/LoginRegisterPage.jsx
@@ -2,8 +2,6 @@ import "./LoginRegisterPage.scss";
 
 // Components
 import Login from "../../components/login/Login";
-import Registeruser from "../../components/register/Registeruser";
-import { Outlet } from "react-router-dom";
 
 const Loginpage = () => {
   return (
